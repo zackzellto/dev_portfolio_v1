fc3aacce423a90cadbb5537ce333200c73118ea2
Zack Zellto <zacharyzellto@gmail.com>
2023-04-23 16:03:19 -0600

    Added a mobile nav panel, still need to transition

--- src/components/word-sphere/navbar/Navbar.tsx
+++ src/components/word-sphere/navbar/Navbar.tsx
@@ -1,22 +1,58 @@
 import Link from 'next/link'
 import React from 'react'
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
 
 
 const Navbar = () => {
+    const [nav, setNav] = React.useState(false)
+
+    const handleNav = () => { 
+        setNav(!nav)
+    }
+
   return (
-    <div><span className='font-poppins text-xl text-white space-x-16 scroll-smooth'>
-        <Link href="/about" >
-            About
-        </Link>
-        <Link href="/portfolio">
-            Portfolio
-        </Link>
-        <Link href="/experience">
-            Experience
-        </Link>
-        <Link href="/">
-            Résumé
-        </Link></span>
+    <div>
+        <div className='flex  text-xl text-white space-x-16 scroll-smooth bg-[#0C0216] px-8 py-8 justify-center cursor-pointer'>
+            <ul className='flex '>
+            <li className='hover:text-[#8A2BE2] p-8'>
+                    <Link href='/about'>About</Link>
+                </li>
+
+                <li className='hover:text-[#8A2BE2] p-8'>
+                    <Link href='/portfolio'>Portfolio</Link>
+                </li>
+                
+                <li className='hover:text-[#8A2BE2] p-8'>
+                    <Link href='/experience'>Experience</Link>
+                </li>
+
+                <li className='hover:text-[#8A2BE2] p-8'>
+                    <Link href='/resume'>Resume</Link>
+                </li>
+            </ul>
+            <div onClick={handleNav} className='block md:hidden'>
+                {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}                
+            </div>
+            <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full' : 'fixed left-[100%]'}>
+                <ul className='flex flex-col justify-center items-center h-full'>
+                    <li className='hover:text-[#8A2BE2] p-8'>
+                        <Link href='/about'>About</Link>
+                    </li>
+
+                    <li className='hover:text-[#8A2BE2] p-8'>
+                        <Link href='/portfolio'>Portfolio</Link>
+                    </li>
+
+                    <li className='hover:text-[#8A2BE2] p-8'>
+                        <Link href='/experience'>Experience</Link>
+                    </li>
+
+                    <li className='hover:text-[#8A2BE2] p-8'>
+                        <Link href='/resume'>Resume</Link>
+                    </li>
+                </ul>
+                </div>
+        </div>
     </div>
   )
 }
