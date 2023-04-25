89f20de767dc9a43703c8adadbd25be035a804fb
Zack Zellto <zacharyzellto@gmail.com>
2023-04-24 20:24:38 -0600

    Nax hamburger menu positioned and functional

--- src/components/word-sphere/navbar/Navbar.tsx
+++ src/components/word-sphere/navbar/Navbar.tsx
@@ -1,54 +1,64 @@
-import Link from 'next/link'
-import React from 'react'
-import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
-
+import Link from "next/link";
+import React from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
-    const [nav, setNav] = React.useState(false)
+  const [nav, setNav] = React.useState(false);
 
-    const handleNav = () => { 
-        setNav(!nav)
-    }
+  const handleNav = () => {
+    setNav(!nav);
+  };
 
   return (
     <div>
-        <div className='flex justify-center items-center text-[22px] text-white bg-[#0C0216] px-8 py-8 cursor-pointer'>
-            <ul className='hidden md:flex '>
-            <li className='hover:text-[#8A2BE2] p-8'>
-                    <Link href='/about'>About</Link>
-                </li>
-                <li className='hover:text-[#8A2BE2] p-8'>
-                    <Link href='/portfolio'>Portfolio</Link>
-                </li>                
-                <li className='hover:text-[#8A2BE2] p-8'>
-                    <Link href='/experience'>Experience</Link>
-                </li>
-                <li className='hover:text-[#8A2BE2] p-8'>
-                    <Link href='/resume'>Resume</Link>
-                </li>
-            </ul>
-        </div>
-        <div onClick={handleNav} className='block md:hidden text-white bg-[#0C0216] px-8 py-8 cursor-pointer'>
-            {nav ? <AiOutlineMenu size={20} /> : <AiOutlineClose size={20} />}                
-        </div>
-            <div className={!nav ? 'fixed w-[30%] text-white h-full' : 'fixed right-[100%]'}>
-                <ul className='flex flex-col justify-center items-center h-full md:hidden'>
-                    <li className='hover:text-[#8A2BE2] p-8 '>
-                        <Link href='/about'>About</Link>
-                    </li>
-                    <li className='hover:text-[#8A2BE2] p-8'>
-                        <Link href='/portfolio'>Portfolio</Link>
-                    </li>
-                    <li className='hover:text-[#8A2BE2] p-8'>
-                        <Link href='/experience'>Experience</Link>
-                    </li>
-                    <li className='hover:text-[#8A2BE2] p-8'>
-                        <Link href='/resume'>Resume</Link>
-                    </li>
-                </ul>
-                </div>
-        </div>
-    )
-}
+      <div className="hidden cursor-pointer items-center justify-center bg-[#0C0216] px-8 py-8 text-[22px] text-white md:flex">
+        <ul className="hidden md:flex ">
+          <li className="p-8 hover:text-[#8A2BE2]">
+            <Link href="/about">About</Link>
+          </li>
+          <li className="p-8 hover:text-[#8A2BE2]">
+            <Link href="/portfolio">Portfolio</Link>
+          </li>
+          <li className="p-8 hover:text-[#8A2BE2]">
+            <Link href="/experience">Experience</Link>
+          </li>
+          <li className="p-8 hover:text-[#8A2BE2]">
+            <Link href="/resume">Resume</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="bg-[#0C0216]">
+        <button
+          onClick={handleNav}
+          className="fixed left-[90%] cursor-pointer bg-[#0C0216] px-16 py-16 text-white md:hidden"
+        >
+          {nav ? <AiOutlineMenu size={20} /> : <AiOutlineClose size={20} />}
+        </button>
+      </div>
+      <div
+        className={
+          !nav
+            ? " absolute left-[70%] top-48 block w-[30%] bg-[#0C0216] text-white"
+            : "fixed left-[100%]"
+        }
+      >
+        <ul className="flex flex-col items-center px-16 md:hidden">
+          <li className="p-8 hover:text-[#8A2BE2] ">
+            <Link href="/about">About</Link>
+          </li>
+          <li className="p-8 hover:text-[#8A2BE2]">
+            <Link href="/portfolio">Portfolio</Link>
+          </li>
+          <li className="p-8 hover:text-[#8A2BE2]">
+            <Link href="/experience">Experience</Link>
+          </li>
+          <li className="p-8 hover:text-[#8A2BE2] ">
+            <Link href="/resume">Resume</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
