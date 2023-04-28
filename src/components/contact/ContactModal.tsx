8ce2627d1160323dfbc61093148fa5b5451d433e
Zack Zellto <zacharyzellto@gmail.com>
2023-04-27 23:53:54 -0600

    restructuring for mobile-first w/ tailwind

---
+++ src/components/contact/ContactModal.tsx
@@ -0,0 +1,32 @@
+// import React from "react";
+
+// const ContactModal = () => {
+//   return (
+//     <div>
+//       <div className="login-box">
+//         <h2>Login</h2>
+//         <form>
+//           <div className="user-box">
+//             <input type="text" name="">
+//               <label>Username</label>
+//             </input>
+//           </div>
+//           <div className="user-box">
+//             <input type="password" name="">
+//               <label>Password</label>
+//             </input>
+//           </div>
+//           <a href="#">
+//             <span></span>
+//             <span></span>
+//             <span></span>
+//             <span></span>
+//             Submit
+//           </a>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ContactModal;
